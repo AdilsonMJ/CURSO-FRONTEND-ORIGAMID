6fd761eb7bca0e01d6fec1072cf2a0c108ab2f81
Adilson Jäger <adilsonmagalhaesjager@gmail.com>
2021-03-27 17:13:24 +0000

    start again

---
+++ JavaScript-ES6/variable.js
@@ -0,0 +1,49 @@
+// var, let and const 
+
+var name = 'Adilson';
+var age = 28;
+var goToCollege = true
+
+console.log(` Name: ${name},  idade: ${age}, Go to College? : ${goToCollege} `)
+
+var surname = "Jäger", city = "Boston"
+console.log(surname, city)
+
+var without;
+console.log(without) // Return underfined because I did not set a attribute
+
+/* Errors
+var function;
+var 1first;
+*/
+
+//valid
+var $selection;
+var _name;
+var goToColleger;
+
+//Var and Let 
+// I can changer the attribute.
+var team = "Toronto ";
+console.log(team) // Return Toronto
+var team = "Miami Heat"
+console.log(team) // Return Miami Heat
+
+let food = "hamburguer"
+//let food = "Ice Cream" // Return Erro and Broken all js
+food = "Ice Cream" 
+console.log(food) // Return Ice Cream
+
+// Training
+
+// => Set a variable with your name and ager 
+          var MyName = "Adilson Jäger" 
+          var MyAger = 27
+
+// => set 5 variable without attribute
+          let number;
+          let ager;
+          let food;
+          let girlFriend;
+          let country;
+
